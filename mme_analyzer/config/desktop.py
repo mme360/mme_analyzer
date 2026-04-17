7968739b27970b3a94a7ddc780f262a2195428f9
mme360 <tawfiq@murtajacorp.com>
2026-04-17 21:24:18 +0300

    Add desktop config

---
+++ mme_analyzer/config/desktop.py
@@ -0,0 +1,11 @@
+from frappe import _
+
+
+def get_data():
+    return [
+        {
+            "module_name": "MME Analyzer",
+            "type": "module",
+            "label": _("MME Analyzer"),
+        }
+    ]
