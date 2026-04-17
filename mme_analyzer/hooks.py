96212a5fa5b8b8322b3a8752cafc75a998b6b34f
mme360 <tawfiq@murtajacorp.com>
2026-04-17 21:21:59 +0300

    Add hooks

---
+++ mme_analyzer/hooks.py
@@ -0,0 +1,9 @@
+app_name = "mme_analyzer"
+app_title = "MME Analyzer"
+app_publisher = "MME360"
+app_description = "Analyzer support and monitoring app for ERPNext"
+app_license = "MIT"
+
+required_apps = ["erpnext"]
+
+fixtures = []
