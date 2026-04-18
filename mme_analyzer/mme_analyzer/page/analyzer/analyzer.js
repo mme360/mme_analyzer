527ed04f69a6442f92069952ad899b9ea7197fab
mme360 <tawfiq@murtajacorp.com>
2026-04-18 15:18:19 +0300

    Use on_page_show redirect for Analyzer desk page in v16

--- mme_analyzer/mme_analyzer/page/analyzer/analyzer.js
+++ mme_analyzer/mme_analyzer/page/analyzer/analyzer.js
@@ -1,3 +1,11 @@
-frappe.pages['analyzer'].on_page_load = function () {
+frappe.pages['analyzer'].on_page_load = function (wrapper) {
+    frappe.ui.make_app_page({
+        parent: wrapper,
+        title: 'Analyzer',
+        single_column: true,
+    });
+};
+
+frappe.pages['analyzer'].on_page_show = function () {
     frappe.set_route('List', 'Analyzer');
 };
